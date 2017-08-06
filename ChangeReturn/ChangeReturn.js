158ad7bcac85bc649fc4bf77f978bbf92746e39d
Angel <rodriguezaa@g.cofc.edu>
2017-08-06 19:11:15 -0400

    added dollar to change

--- ChangeReturn/ChangeReturn.js
+++ ChangeReturn/ChangeReturn.js
@@ -9,6 +9,7 @@ var qIMG = document.getElementById("changeIMG").childNodes[1];
 var dIMG = document.getElementById("changeIMG").childNodes[3];
 var nIMG = document.getElementById("changeIMG").childNodes[5];
 var pIMG = document.getElementById("changeIMG").childNodes[7];
+var dollIMG = document.getElementById("changeIMG").childNodes[9];
 
 
 // clears images of coins given
@@ -33,16 +34,17 @@ function drawCoin(coin, numCoins){
 }
 
 function changeReturn(){
-    input = document.getElementById("input").innerHTML;
+    input = document.getElementById("input-given").innerHTML;
     cost = document.getElementById("cost").innerHTML;
 
-
+    var dollar = {name:"dollar", value:1, image: dollIMG};
     var quarter = { name: "quarter", value:0.25, image: qIMG};
     var dime = {name: "dime", value: 0.10, image: dIMG};
     var nickel = {name: "nickel", value: 0.05, image: nIMG};
     var penny = {name: "pennie", value: 0.01, image: pIMG};
-    var change =((input*10 - cost*10)/10);
+
     var numDollars = Math.floor(input - cost);
+    var change =((input*10 - cost*10) - numDollars*10)/10;
     var numQuarters = 4 *  Math.floor(input % 1);
     var numDimes = 0;
     var numNickles = 0;
@@ -50,43 +52,47 @@ function changeReturn(){
     
     //  clear old coin images
 
-
+    clearIMG(dollar);
     clearIMG(quarter);
     clearIMG(dime);
     clearIMG(nickel);
     clearIMG(penny);
 
+    drawCoin(dollar,numDollars);
 
     while(change > .009){
         if(change >= quarter.value){
             temp = (change*10 - quarter.value *10);
-            change -= quarter.value;
+            change = temp/10 ;
             numQuarters++;
         }
 
         else if(change >= dime.value){
-            
-            change -= dime.value;
+            temp = (change*10 - dime.value *10);
+            change = temp/10;
+            alert(change);
             numDimes++;
         }
 
         else if(change >= nickel.value){
-            change -= nickel.value;
+            alert(change);
+            temp = (change*10 - nickel.value *10);
+            change = temp/10;
             numNickles++;
         }
 
         else{
-            change -= penny.value;
+            temp = (change*10 - penny.value *10);
+            change = temp/10;
             numPennies++;
         }
 
-        document.getElementById("output").innerHTML ="Change " + ((input*10 - cost*10)/10)  + " Dollars: " + numDollars + " Quarters: " +
+        document.getElementById("output-change").innerHTML ="Change " + ((input*10 - cost*10)/10)  + " Dollars: " + numDollars + " Quarters: " +
          numQuarters + " Dimes: " + numDimes + " Nickles: " + numNickles + " Pennies: " + numPennies;
 
     }
 
     // draw coins
-
         drawCoin(quarter, numQuarters);
         drawCoin(dime, numDimes);
         drawCoin(nickel, numNickles);
